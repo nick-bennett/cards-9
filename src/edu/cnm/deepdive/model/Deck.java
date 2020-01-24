86f4aab6f54d8ee247a469145984c8bce6d511b1
Nick Bennett <nick@nickbenn.com>
2020-01-24 11:29:27 -0700

    Completed unit tests.

--- src/edu/cnm/deepdive/model/Deck.java
+++ src/edu/cnm/deepdive/model/Deck.java
@@ -36,6 +36,14 @@ public class Deck {
     Collections.shuffle(cards, rng);
   }
 
+  public int remaining() {
+    return cards.size();
+  }
+
+  public int dealt() {
+    return dealt.size();
+  }
+
   @Override
   public String toString() {
     return cards.toString();
