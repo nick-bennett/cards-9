ec65f6afd1d96d28905ba9a28437e184f4eba81f
Nick Bennett <nick@nickbenn.com>
2020-01-24 10:28:51 -0700

    Tests for Card.

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,13 +1,17 @@
 package edu.cnm.deepdive.model;
 
+import java.util.Objects;
+
 public class Card {
 
   private final Suit suit;
   private final Rank rank;
+  private final int hash;
 
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
+    hash = Objects.hash(suit, rank);
   }
 
   public Suit getSuit() {
@@ -23,4 +27,23 @@ public class Card {
     return rank.symbol() + suit.symbol();
   }
 
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean comparison = false;
+    if (obj == this) {
+      comparison = true;
+    } else if (obj instanceof Card) {
+      Card other = (Card) obj;
+      if (hash == other.hash && suit == other.suit && rank == other.rank) {
+        comparison = true;
+      }
+    }
+    return comparison;
+  }
+  
 }
