b128bf8412270ca03577cb460d958584578ace08
Nick Bennett <nick@nickbenn.com>
2020-01-27 14:15:55 -0700

    Defined JAR and started Javadoc.

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -2,22 +2,40 @@ package edu.cnm.deepdive.model;
 
 import java.util.Objects;
 
+/**
+ * Encapsulates a single playing card as a combination of {@link Suit} and {@link Rank}. Instances
+ * of this class are immutable.
+ *
+ * @author Nicholas Bennett &amp; Deep Dive Coding Java + Android Cohort 9.
+ */
 public class Card {
 
   private final Suit suit;
   private final Rank rank;
   private final int hash;
 
+  /**
+   * Initializes the Card instance with the specified {@link Suit} and {@link Rank}.
+   *
+   * @param suit {@link Suit} value of card.
+   * @param rank {@link Rank} value of card.
+   */
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
     hash = Objects.hash(suit, rank);
   }
 
+  /**
+   * Returns {@link Suit} of this Card instance.
+   */
   public Suit getSuit() {
     return suit;
   }
 
+  /**
+   * Returns {@link Rank} of this Card instance.
+   */
   public Rank getRank() {
     return rank;
   }
--- src/edu/cnm/deepdive/model/Suit.java
+++ src/edu/cnm/deepdive/model/Suit.java
@@ -1,5 +1,10 @@
 package edu.cnm.deepdive.model;
 
+/**
+ * Encapsulates the suits of standard playing cards. The enum includes {@link #symbol()} and {@link
+ * #color()} methods, to return the immutable Unicode symbol and {@link Color} enum instance of any
+ * suit.
+ */
 public enum Suit {
 
   CLUBS, DIAMONDS, HEARTS, SPADES;
@@ -7,16 +12,29 @@ public enum Suit {
   private static final String[] symbols = {"\u2663", "\u2662", "\u2661", "\u2660"};
   private static final Color[] colors = {Color.BLACK, Color.RED, Color.RED, Color.BLACK};
 
+  /**
+   * Returns Unicode playing card symbol for this suit.
+   *
+   * @return Unicode symbol.
+   */
   public String symbol() {
     return symbols[ordinal()];
   }
 
+  /**
+   * Returns {@link Color} instance value for this suit.
+   *
+    * @return {@link Color} value.
+   */
   public Color color() {
     return colors[ordinal()];
   }
 
+  /**
+   * Encapsulates the two possible colors (red &amp; black) of playing card suits.
+   */
   public enum Color {
-    RED, BLACK;
+    RED, BLACK
   }
 
 }
