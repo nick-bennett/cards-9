5dbfee9146151459fcd37a5ecf6ec1da66b27ca3
Nick Bennett <nick@nickbenn.com>
2020-02-27 10:23:34 -0700

    Deck implements Comparator.

--- src/edu/cnm/deepdive/model/Deck.java
+++ src/edu/cnm/deepdive/model/Deck.java
@@ -28,7 +28,7 @@ import java.util.Random;
  *
  * @author Nicholas Bennett &amp; Deep Dive Coding Java + Android Cohort 9.
  */
-public class Deck {
+public class Deck implements Comparator<Card> {
 
   private List<Card> cards;
   private List<Card> dealt;
@@ -100,7 +100,12 @@ public class Deck {
     if (gather) {
       gather();
     }
-    cards.sort(Comparator.comparing(Card::getSuit).thenComparing(Card::getRank));
+    cards.sort(this);
+  }
+
+  @Override
+  public int compare(Card card1, Card card2) {
+    return Comparator.comparing(Card::getSuit).thenComparing(Card::getRank).compare(card1, card2);
   }
 
 }
